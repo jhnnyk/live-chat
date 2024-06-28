c4a612e0faff591470695697c3bed13f69d9726b
John Knotts <jhnnyk@gmail.com>
2024-06-27 20:43:28 -0500

    base project setup

---
+++ src/views/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
