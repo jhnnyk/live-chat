5e65701d91bfb361dc7a0f7df878607a683a7c50
John Knotts <jhnnyk@gmail.com>
2024-07-01 14:12:00 -0600

    login composable and user login with error messages

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,16 @@
 <script setup>
+import useLogin from '@/composables/useLogin'
+
 const email = defineModel('email')
 const password = defineModel('password')
 
-const handleSubmit = () => {
-  console.log(email.value, password.value)
+const { error, login } = useLogin()
+
+const handleSubmit = async () => {
+  await login(email.value, password.value)
+  if (!error.value) {
+    console.log('user logged in')
+  }
 }
 </script>
 
@@ -11,6 +18,7 @@ const handleSubmit = () => {
   <form @submit.prevent="handleSubmit">
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button>Log in</button>
   </form>
 </template>
---
+++ src/composables/useLogin.js
@@ -0,0 +1,25 @@
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { projectAuth } from '@/firebase/config'
+import { ref } from 'vue'
+
+const error = ref(null)
+
+const login = async (email, password) => {
+  error.value = null
+
+  try {
+    const res = await signInWithEmailAndPassword(projectAuth, email, password)
+    error.value = null
+    console.log(res)
+    return res
+  } catch (err) {
+    console.log(err.value)
+    error.value = 'Incorrect login credentials'
+  }
+}
+
+const useLogin = () => {
+  return { error, login }
+}
+
+export default useLogin
