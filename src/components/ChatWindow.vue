1f6c904a190514105e38ebdf30b36c94f8e5335a
John Knotts <jhnnyk@gmail.com>
2024-07-02 12:25:56 -0600

    auto-scrolling the chat window

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <script setup>
 import getCollection from '@/composables/getCollection'
 import { formatDistanceToNow } from 'date-fns'
-import { computed } from 'vue'
+import { computed, onUpdated, ref } from 'vue'
 
 const { error, documents } = getCollection('messages')
 
@@ -17,12 +17,19 @@ const formattedDocuments = computed(() => {
 
   return newDocs
 })
+
+// auto-scroll to bottom of messages
+const messages = ref(null)
+
+onUpdated(() => {
+  messages.value.scrollTop = messages.value.scrollHeight
+})
 </script>
 
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
+    <div v-if="documents" class="messages" ref="messages">
       <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
         <span class="created-at">{{ doc.createdAt }}</span>
         <span class="name">{{ doc.name }}</span>
