5983fa84375854b0021088771eb255d872bc90c9
John Knotts <jhnnyk@gmail.com>
2024-07-02 12:43:11 -0600

    route guard

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,19 @@ import { projectAuth } from '@/firebase/config'
 // auth guard
 const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser
-  console.log('current user in auth guard: ', user)
+
   if (!user) {
     next({ name: 'WelcomeView' })
+  } else {
+    next()
+  }
+}
+
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+
+  if (user) {
+    next({ name: 'ChatroomView' })
   } else {
     next()
   }
@@ -20,7 +30,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'WelcomeView',
-      component: WelcomeView
+      component: WelcomeView,
+      beforeEnter: requireNoAuth
     },
     {
       path: '/chatroom',
