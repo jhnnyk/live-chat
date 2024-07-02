7dd50d97cef20c17e36b345a56c69a4999923a6c
John Knotts <jhnnyk@gmail.com>
2024-07-02 09:37:26 -0600

    watching the current user

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -1,5 +1,17 @@
 <script setup>
+import getUser from '@/composables/getUser'
 import NavBar from '../components/NavBar.vue'
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
+
+const router = useRouter()
+const { user } = getUser()
+
+watch(user, () => {
+  if (!user.value) {
+    router.push({ name: 'WelcomeView' })
+  }
+})
 </script>
 
 <template>
